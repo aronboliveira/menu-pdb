088fba5d13b1a1a6db75136df980d5ff505e6201
Aron Barbosa de Oliveira <aron.b.96@gmail.com>
2025-09-01 22:23:23 -0300

    Import print styles in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
 import Image from "next/image";
+import "@/styles/print.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
